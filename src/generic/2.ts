fc09d9adf38d778e309a3d8fa86d9cbed141eed2
sivlvy <vladislavsidenko@ukr.net>
2024-02-21 19:00:08 +0200

    update

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -5,19 +5,22 @@
 */
 
 type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number
-}
+	name: string;
+	position: number;
+	color: string;
+	weight: number;
+};
 
-function compare (top, bottom): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
+function compare<T extends AllType>(
+	top: Pick<T, keyof AllType>,
+	bottom: Pick<T, keyof AllType>
+): AllType {
+	return {
+		name: top.name,
+		color: top.color,
+		position: bottom.position,
+		weight: bottom.weight,
+	};
 }
 
 export {};
